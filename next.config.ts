53c9d9a481ef176c613742d05b8af843cc618ba6
jphechter <joseph.hechter@gmail.com>
2025-04-04 23:22:26 -0400

    updates to build artifacts config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/wedding-website",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
